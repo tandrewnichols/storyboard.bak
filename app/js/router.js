5611bec5d154baaa398fdb849ca5b383d6baf06a
Andrew Nichols <tandrewnichols@gmail.com>
2014-12-30 10:14:35 -0500

    Blah, forget changes for now

--- app/js/router.js
+++ app/js/router.js
@@ -1,6 +1,12 @@
 angular.module('app').config(function($locationProvider, $stateProvider, $urlRouterProvider) {
   $locationProvider.html5Mode(true);
-  //$urlRouterProvider.otherwise('/');
+  //$urlRouterProvider.otherwise(function($injector, $location) {
+    //if (_(['/logout']).contains($location.path())) {
+      //window.location.replace($location.absUrl());
+    //} else {
+      //return '/';
+    //}
+  //});
   $stateProvider
     .state('home', {
       url: '/',
